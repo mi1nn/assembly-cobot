874f99873235699f9ea4980a51ccb1d942d34c8c
hymi_rust <lhm6582@gmail.com>
2026-08-21 18:01:42 +0900

    modify func pick, place

--- src/solar_panel_robot/solar_panel_robot/motion.py
+++ src/solar_panel_robot/solar_panel_robot/motion.py
@@ -9,8 +9,8 @@ from DSR_ROBOT2 import (
     posx,
     trans,
     DR_BASE,
+    DR_MV_MOD_REL,
 )
-
 
 
 class RobotMotion:
@@ -69,11 +69,20 @@ class RobotMotion:
 
         print("[HOME] 이동 완료", flush=True)
 
-    def pick(self, pose_name, approach_height=50):
-        target, ready = self.make_target_ready(
-            pose_name,
-            approach_height
+    def move_z(self, distance):
+        # BASE 좌표계 기준 Z축 상대 이동
+        # distance > 0 : 상승
+        # distance < 0 : 하강
+
+        movel(
+            posx(0, 0, distance, 0, 0, 0),
+            vel=self.velocity,
+            acc=self.acc,
+            ref=DR_BASE,
+            mod=DR_MV_MOD_REL
         )
+
+    def pick(self, distance=50):
 
         print(
             f"[PICK] velocity={self.velocity}, "
@@ -81,86 +90,30 @@ class RobotMotion:
             flush=True
         )
 
-        print("[PICK] ready :", ready, flush=True)
-        print("[PICK] target:", target, flush=True)
+        # 1. BASE Z 방향으로 하강
+        self.move_z(-distance)
 
-
-        # movel(
-        #     ready,
-        #     vel=self.velocity,
-        #     acc=self.acc
-        # ) # pick 장소로 이동
-
-        # Pick 위치 위로 이동
-        movel(
-            ready,
-            vel=self.velocity,
-            acc=self.acc
-        )
-
-        # Pick 위치로 하강
-        movel(
-            target,
-            vel=self.velocity,
-            acc=self.acc
-        )
-
-        # Gripper Close
+        # 2. Gripper Close
         self.gripper.grasp()
 
-        # 다시 상승
-        movel(
-            ready,
-            vel=self.velocity,
-            acc=self.acc
-        )
+        # 3. BASE Z 방향으로 상승
+        self.move_z(distance)
 
-    def place(self, pose_name, approach_height=100):
-        target, ready = self.make_target_ready(
-            pose_name,
-            approach_height
-        )
-
+    def place(self, distance=50):
         print(
             f"[PLACE] velocity={self.velocity}, "
             f"acc={self.acc}",
             flush=True
         )
 
-        print("[PLACE] ready :", ready, flush=True)
-        print("[PLACE] target:", target, flush=True)
+        # 1. BASE Z 방향으로 하강
+        self.move_z(-distance)
 
-
-        # movel(
-        #     ready,
-        #     vel=self.velocity,
-        #     acc=self.acc
-        # ) # place 장소로 이동
-
-        # Place 위치 위로 이동
-        movel(
-            ready,
-            vel=self.velocity,
-            acc=self.acc
-        )
-
-
-        # Place 위치로 하강
-        movel(
-            target,
-            vel=self.velocity,
-            acc=self.acc
-        )
-
-        # Gripper Open
+        # 2. Gripper Open
         self.gripper.release()
 
-        # 다시 상승
-        movel(
-            ready,
-            vel=self.velocity,
-            acc=self.acc
-        )
+        # 3. BASE Z 방향으로 상승
+        self.move_z(distance)
 
     def move_arc(start, end, height=100, steps=6):
 
