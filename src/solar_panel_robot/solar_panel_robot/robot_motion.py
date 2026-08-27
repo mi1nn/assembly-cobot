c5aee80328d98647ff20512aabdff795fb007dac
jb9874123 <jb9874123@gmail.com>
2026-08-27 14:03:29 +0900

    feat: 오전 작업 마무리

--- src/solar_panel_robot/solar_panel_robot/robot_motion.py
+++ src/solar_panel_robot/solar_panel_robot/robot_motion.py
@@ -35,7 +35,7 @@ ON = 1
 # 작업별 speed/acc/좌표/Force 값은 SolarMotion이 DB에서 받아 각 함수에 전달한다.
 # 아래 값은 로봇 공통 기본값과 초기 Home/Gripper 설정에만 사용한다.
 DEFAULT_VELOCITY = 50.0
-DEFAULT_ACC = 100.0
+DEFAULT_ACC = 50.0
 HOME_POSITION = [0.0, 0.0, 90.0, 0.0, 90.0, 0.0]
 GRASP_PORT = 1
 RELEASE_PORT = 2
